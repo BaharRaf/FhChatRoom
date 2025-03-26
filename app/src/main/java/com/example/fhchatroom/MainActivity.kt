b4e1fb5e6d48ad9bf1abd77d7826dcac76a9c6fe
Sepidehri <sepidriahi@gmail.com>
2025-03-26 17:11:24 +0100

    MainActivity and Theme added

--- app/src/main/java/com/example/fhchatroom/MainActivity.kt
+++ app/src/main/java/com/example/fhchatroom/MainActivity.kt
@@ -1,29 +1,41 @@
 package com.example.fhchatroom
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.fhchatroom.ui.theme.FhChatRoomTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+import com.example.fhchatroom.screen.LoginScreen
+import com.example.fhchatroom.screen.SignUpScreen
+import com.example.fhchatroom.ui.theme.ChatRoomAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            FhChatRoomTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+
+            ChatRoomAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
                 }
             }
         }
@@ -31,17 +43,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NavigationGraph(
+    navController: NavHostController,
+
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SignupScreen.route
+    ) {
+        composable(Screen.SignupScreen.route) {
+            SignUpScreen(
+
+                onNavigateToLogin = { navController.navigate(Screen.LoginScreen.route) }
+            )
+        }
+
+        }
+
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FhChatRoomTheme {
-        Greeting("Android")
+    ChatRoomAppTheme {
+
     }
 }
