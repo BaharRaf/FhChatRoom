041d6e73df97a79dd32e9cd19a21cfff334374d7
BaharRaf <114486248+BaharRaf@users.noreply.github.com>
2025-03-26 15:38:47 +0100

    Build_gradle update, google-services added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -56,4 +58,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Import firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
+
+
 }
