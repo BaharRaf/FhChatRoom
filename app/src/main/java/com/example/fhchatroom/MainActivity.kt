0be9f54dbb1f37f916ce0725abc00697178c33c5
BaharRaf <114486248+BaharRaf@users.noreply.github.com>
2025-03-26 17:18:49 +0100

    NavHost controller

--- app/src/main/java/com/example/fhchatroom/MainActivity.kt
+++ app/src/main/java/com/example/fhchatroom/MainActivity.kt
@@ -7,11 +7,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -44,8 +42,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun NavigationGraph(
-    navController: NavHostController,
-
+    navController: NavHostController
 ) {
     NavHost(
         navController = navController,
@@ -53,16 +50,18 @@ fun NavigationGraph(
     ) {
         composable(Screen.SignupScreen.route) {
             SignUpScreen(
-
                 onNavigateToLogin = { navController.navigate(Screen.LoginScreen.route) }
             )
         }
-
+        composable(Screen.LoginScreen.route) {
+            LoginScreen(
+                onNavigateToSignUp = { navController.navigate(Screen.SignupScreen.route) }
+            )
         }
-
-
-
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
