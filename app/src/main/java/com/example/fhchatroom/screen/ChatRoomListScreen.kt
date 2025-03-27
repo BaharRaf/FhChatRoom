d2ecb31783aa20d092b3a3654f3c5d780a71de42
Sepidehri <sepidriahi@gmail.com>
2025-03-27 15:34:32 +0100

    RoomViewModel access given to ChatRoomListScreen

--- app/src/main/java/com/example/fhchatroom/screen/ChatRoomListScreen.kt
+++ app/src/main/java/com/example/fhchatroom/screen/ChatRoomListScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,11 +20,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.fhchatroom.Screen
+import com.example.fhchatroom.data.Room
 import com.example.fhchatroom.ui.theme.ChatRoomAppTheme
+import com.example.fhchatroom.viewmodel.RoomViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ChatRoomListScreen() {
+fun ChatRoomListScreen(roomViewModel: RoomViewModel = viewModel(),
+                       onJoinClicked: (Room) -> Unit
+) {
+
     var showDialog by remember { mutableStateOf(false) }
     var name by remember { mutableStateOf("") }
 
