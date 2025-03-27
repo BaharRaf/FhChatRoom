6508c3a402675224acb26523b9cabe25cf316f3f
Sepidehri <sepidriahi@gmail.com>
2025-03-27 14:23:04 +0100

    AuthViewModel.kt added

--- app/src/main/java/com/example/fhchatroom/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/fhchatroom/viewmodel/AuthViewModel.kt
@@ -1,3 +1,37 @@
 package com.example.fhchatroom.viewmodel
 
+import com.example.fhchatroom.data.UserRepository
+import com.example.fhchatroom.data.Result
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.example.fhchatroom.Injection
+import kotlinx.coroutines.launch
 
+class AuthViewModel : ViewModel() {
+    private val userRepository: UserRepository
+
+    init {
+        userRepository = UserRepository(
+            FirebaseAuth.getInstance(),
+            Injection.instance()
+        )
+    }
+
+    private val _authResult = MutableLiveData<Result<Boolean>>()
+    val authResult: LiveData<Result<Boolean>> get() = _authResult
+
+    fun signUp(email: String, password: String, firstName: String, lastName: String) {
+        viewModelScope.launch {
+            _authResult.value = userRepository.signUp(email, password, firstName, lastName)
+        }
+    }
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            _authResult.value = userRepository.login(email, password)
+        }
+    }
+}
