88d0b95636dad5f4d5ded942c9016a218102a6e0
Sepidehri <sepidriahi@gmail.com>
2025-05-27 12:26:24 +0200

    owneremail in room.kt added

--- app/src/main/java/com/example/fhchatroom/data/Room.kt
+++ app/src/main/java/com/example/fhchatroom/data/Room.kt
@@ -4,5 +4,6 @@ data class Room(
     val id: String = "",
     val name: String = "",
     val description: String = "",
-    val members: List<String> = emptyList()
+    val members: List<String> = emptyList(),
+    val ownerEmail: String = ""
 )
